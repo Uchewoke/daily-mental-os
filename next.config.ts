8ba561c7d5171a8599f6b215909129151ab6494a
uchewoke <cheuche87@gmail.com>
2026-06-29 16:32:36 -0400

    Description of what you changed

--- next.config.ts
+++ next.config.ts
@@ -5,3 +5,10 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+import path from 'path';
+
+const nextConfig = {
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname),
+  },
+};
