2025281f89cfd4a677f88c89dac81d2619d90c2a
uchewoke <cheuche87@gmail.com>
2026-06-29 17:00:29 -0400

    Description of what you changed

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,8 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  outputFileTracingRoot: path.join(__dirname),
 };
 
 export default nextConfig;
-import path from 'path';
-
-const nextConfig = {
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname),
-  },
-};
